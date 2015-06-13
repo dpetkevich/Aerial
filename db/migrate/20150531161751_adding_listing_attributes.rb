4e5e1cb94c6be8c6f7b08e2e455649ae2453d4df
Daniel Petkevich <danielpetkevich@Daniels-MacBook-Pro.local>
2015-06-13 11:42:24 -0700

    fixing migration

--- db/migrate/20150531161751_adding_listing_attributes.rb
+++ db/migrate/20150531161751_adding_listing_attributes.rb
@@ -2,6 +2,6 @@ class AddingListingAttributes < ActiveRecord::Migration
   def change
   	add_column :listings, :review_count, :integer
   	add_column :listings, :review_score, :decimal
-  	add_column :listings, :tags, :string, :array: true
+  	add_column :listings, :tags, :string, array: :true
   end
 end
